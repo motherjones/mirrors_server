69e3f028dc59968976230939d81a4505bb8a2c69
Mikela Clemmons <glassresistor@gmail.com>
2014-04-17 14:25:22 -0700

    switched to django coverage

--- mirrors_server/settings.py
+++ mirrors_server/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_coverage',
     'rest_framework',
     'mirrors_server', 
     'mirrors',
