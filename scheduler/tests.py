bb996d4e2cd2f95122ad17c7e415583b5680c19d
Cameron Adamez <cameron@soycow.org>
2014-05-07 15:12:58 -0700

    pep8

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,9 +1,9 @@
-from django.test import TestCase
-from django.test.client import RequestFactory, Client
-from scheduler.models import * 
-from mirrors.models import *
-from datetime import datetime, timedelta
 import json
+from django.test import TestCase
+from django.test.client import Client
+from scheduler.models import Reservation
+from datetime import datetime, timedelta
+
 
 class ReservationRequestTest(TestCase):
     def setUp(self):
@@ -20,51 +20,69 @@ class ReservationRequestTest(TestCase):
 
     def test_make_reservation(self):
         response = self.client.post('/scheduler',
-                {'slug': 'example', 'datetime': datetime.now().isoformat()})
+                                    {'slug': 'example',
+                                     'datetime': datetime.now().isoformat()})
         self.assertEqual(response.status_code, 200)
+
     def test_make_reservation_invalid_slug(self):
+        date = datetime.now().isoformat()
         response = self.client.post('/scheduler',
-                {'slug': 'nope!!!', 'datetime': datetime.now().isoformat()})
+                                    {
+                                        'slug': 'nope!!!',
+                                        'datetime': date
+                                    })
         self.assertEqual(response.status_code, 400)
+
     def test_make_reservation_invalid_date(self):
+        date = datetime(1990, 1, 1).isoformat()
         response = self.client.post('/scheduler',
-                {'slug': 'example',
-                 'datetime': datetime(1990,1,1).isoformat()})
+                                    {
+                                        'slug': 'example',
+                                        'datetime': date
+                                    })
         self.assertEqual(response.status_code, 400)
-
-
 
     def test_change_reservation(self):
         r = Reservation(slug='example', datetime=self.teh_future)
         r.save()
-        response = self.client.patch('/scheduler/%s' % r.id,
-                {'slug': 'example',
-                 'datetime': datetime.now().isoformat(),
-                 'reservation': r.id})
+        response = self.client.patch(
+            '/scheduler/%s' % r.id,
+             {
+                'slug': 'example',
+                'datetime': datetime.now().isoformat(),
+                'reservation': r.id
+             })
         self.assertEqual(response.status_code, 200)
+
     def test_change_reservation_invalid_slug(self):
         r = Reservation(slug='example', datetime=self.teh_future)
         r.save()
         response = self.client.patch('/scheduler/%s' % r.id,
-                {'slug': 'notexample',
-                 'datetime': datetime.now().isoformat(),
-                 'reservation': r.id})
+                                    {
+                                        'slug': 'notexample',
+                                        'datetime': datetime.now().isoformat(),
+                                        'reservation': r.id
+                                    })
         self.assertEqual(response.status_code, 400)
+
     def test_change_reservation_invalid_date(self):
         r = Reservation(slug='example', datetime=self.teh_future)
         r.save()
         past_time = (timedelta(days=-10) + datetime.now()).isoformat()
         response = self.client.patch('/scheduler/%s' % r.id,
-                {'slug': 'example',
-                 'datetime': past_time,
-                 'reservation': r.id})
+                {
+                    'slug': 'example',
+                    'datetime': past_time,
+                    'reservation': r.id
+                })
         self.assertEqual(response.status_code, 400)
+
     def test_delete_reservation(self):
         r = Reservation(slug='example', datetime=self.teh_future)
         r.save()
         response = self.client.delete('/scheduler/%s' % r.id)
         self.assertEqual(response.status_code, 204)
+
     def test_delete_reservation_invalid_slug(self):
         response = self.client.delete('/scheduler/squoodlydoo')
         self.assertEqual(response.status_code, 400)
-
